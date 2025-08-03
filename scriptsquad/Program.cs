f493903c2062ea6f971a7f189e46871764c3416a
Miriam <mchenc3@miumg.edu.gt>
2025-08-03 04:44:35 -0600

    :truck: Implementé la función  de multiplicación

--- scriptsquad/Program.cs
+++ scriptsquad/Program.cs
@@ -12,7 +12,30 @@ class Program
         Console.Write("Seleccione una opción: ");
 
         int opcion = int.Parse(Console.ReadLine());
+        
         // TODO: Implementar la lógica de menú
+
+        switch (opcion)
+        {
+            case 3:
+            Multiplicar();
+            break;
+            default:
+            Console.WriteLine("Opciíon no válida o no implementada aún.");
+            break;
+        }
+    }
+
+    static void Multiplicación()
+    {
+        Console.Write("Ingrese el primer número: ");
+        double num1 = double.parse(Console.ReadLine());
+
+        Console.Write("Ingrese el Segundo número: ");
+        double num2 = double.parse(Console.ReadLine());
+
+        double resultado = num1 * num2;
+        Console.Write($"El resultado de la multiplicación es: (resultado)");
     }
 
     // TODO: Implementar funciones de suma, resta, multiplicación, división
